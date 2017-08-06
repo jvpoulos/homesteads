3341ed7a688d8a477ab3b120619c548be5b5d912
jvpoulos <jvpoulos@gmail.com>
2017-08-05 20:12:22 -0400

    do work

--- code/patents.R
+++ code/patents.R
@@ -54,7 +54,12 @@ patents$homesteads[patents$authority_code==251101] <- 1
 # Date to time
 
 patents$date <- as.yearmon(patents$signature_date, "%m/%d/%Y",tz="UTC") # convert to monthly data
-patents <- patents[!is.na(patents$date) & !patents$date>2017,] # subset to non-missing and valid dates ## SAVED HERE
+patents <- patents[!is.na(patents$date) & !patents$date>2017,] # subset to non-missing and valid dates
+
+# Save workspace
+save.image(data.directory, "patents/patents.RData")
+
+#############################################
 
 # Summarize by date/county/state
 
@@ -68,24 +73,26 @@ patents.sum <- patents %>%
 # Category
 southern.pub <- c("AL", "AR", "FL", "LA", "MS") # 5 southern public land states
 
-patents.sum$cat <- ifelse(patents.sum$state_code %in% southern.pub, "S", "NS") 
+patents.sum$cat <- ifelse(patents.sum$state_code %in% southern.pub, "Treated", "Control") 
 
 # Create control and treated sums
 cats.sums <- patents.sum %>% 
   group_by(date,cat) %>% 
   summarise_each(funs(sum),sales,homesteads) 
 
-cats.sums.r <- reshape(data.frame(cats.sums.r), idvar = "date", timevar = "cat", direction = "wide")
+cats.sums.r <- reshape(data.frame(cats.sums), idvar = "date", timevar = "cat", direction = "wide")
+
+## Analysis 1: Effect on treated (south), intervention: June 1876; period: June 1866 - Mar 1889
 
 # Reshape long to wide
 
 patents.sum$id <- interaction(patents.sum$county_code,patents.sum$state_code)
 
-patents.sum <- patents.sum[patents.sum$cat=="Control",] # discard treated since we have treated time-series
+patents.sum.control <- patents.sum[patents.sum$cat=="Control",] # discard treated since we have treated time-series
 
-homesteads <- reshape(data.frame(patents.sum[c("date","id","homesteads")]), idvar = "date", timevar = "id", direction = "wide")
+homesteads <- reshape(data.frame(patents.sum.control[c("date","id","homesteads")]), idvar = "date", timevar = "id", direction = "wide")
 
-sales <- reshape(data.frame(patents.sum[c("date","id","sales")]), idvar = "date", timevar = "id", direction = "wide")
+sales <- reshape(data.frame(patents.sum.control[c("date","id","sales")]), idvar = "date", timevar = "id", direction = "wide")
 
 # Labels
 
@@ -97,17 +104,17 @@ sales.y <- sales.y[!is.na(sales.y$sales.Treated),]
 
 # Splits
 
-homesteads.x.train <- homesteads[homesteads$date %in% homesteads.y$date & homesteads$date < "Jul 1901",]
-homesteads.x.test <- homesteads[homesteads$date %in% homesteads.y$date & homesteads$date >= "Jul 1901",]
+homesteads.y.train <- homesteads.y[homesteads.y$date >= "Jun 1866" & homesteads.y$date <= "May 1876",]
+homesteads.y.test <- homesteads.y[homesteads.y$date >= "Jun 1876"  & homesteads.y$date <= "Mar 1889",]
 
-homesteads.y.train <- homesteads.y[homesteads.y$date < "Jul 1901",]
-homesteads.y.test <- homesteads.y[homesteads.y$date >= "Jul 1901",]
+homesteads.x.train <- homesteads[homesteads$date %in% homesteads.y.train$date & homesteads$date >= "Jun 1866" & homesteads$date <= "May 1876",]
+homesteads.x.test <- homesteads[homesteads$date %in% homesteads.y.test$date & homesteads$date >= "Jun 1876"  & homesteads$date <= "Mar 1889",]
 
-sales.x.train <- sales[sales$date %in% sales.y$date & sales$date < "Jul 1901",]
-sales.x.test <- sales[sales$date %in% sales.y$date & sales$date >= "Jul 1901",]
+sales.y.train <- sales.y[sales.y$date >= "Jun 1866" & sales.y$date <= "May 1876",]
+sales.y.test <- sales.y[sales.y$date >= "Jun 1876" & sales.y$date <= "Mar 1889",]
 
-sales.y.train <- sales.y[sales.y$date < "Jul 1901",]
-sales.y.test <- sales.y[sales.y$date >= "Jul 1901",]
+sales.x.train <- sales[sales$date %in% sales.y.train$date & sales$date >= "Jun 1866" & sales$date <= "May 1876",]
+sales.x.test <- sales[sales$date %in% sales.y.test$date & sales$date >= "Jun 1876" & sales$date <= "Mar 1889",]
 
 # Preprocess
 homesteads.pre.train <- preProcess(homesteads.x.train[!colnames(homesteads.x.train) %in% c("date")], method = c("center", "scale","medianImpute"))
@@ -121,7 +128,7 @@ sales.x.train[!colnames(sales.x.train) %in% c("date")] <- predict(sales.pre.trai
 sales.x.test[!colnames(sales.x.test) %in% c("date")] <- predict(sales.pre.train, sales.x.test[!colnames(sales.x.test) %in% c("date")] ) # use training values for test set 
 
 # Export each as csv (labels, features)
-data.directory <- "~/Dropbox/github/drnns-prediction/data/patents/"
+data.directory <- "~/Dropbox/github/drnns-prediction/data/patents-public/analysis-12/treated/"
 
 write.csv(homesteads.x.train[!colnames(homesteads.x.train) %in% c("date")], paste0(data.directory,"homesteads-x-train.csv"), row.names=FALSE) 
 write.csv(homesteads.x.test[!colnames(homesteads.x.test) %in% c("date")], paste0(data.directory,"homesteads-x-test.csv"), row.names=FALSE) 
@@ -133,45 +140,174 @@ write.csv(sales.x.test[!colnames(sales.x.test) %in% c("date")] , paste0(data.dir
 write.csv(sales.y.train[!colnames(sales.y.train) %in% c("date")], paste0(data.directory,"sales-y-train.csv"), row.names=FALSE) 
 write.csv(sales.y.test[!colnames(sales.y.test) %in% c("date")], paste0(data.directory,"sales-y-test.csv"), row.names=FALSE) 
 
-## Placebo (set intervention 1 year earlier)
+## Analysis 2: Effect on controls (non-south), intervention: June 1876; period: June 1866 - Mar 1889
 
-# Splits
+# Reshape long to wide
 
-homesteads.x.train.p <- homesteads[homesteads$date %in% homesteads.y$date & homesteads$date < "Jul 1898",]
-homesteads.x.test.p <- homesteads[homesteads$date %in% homesteads.y$date & homesteads$date >= "Jul 1898",]
+patents.sum.treated <- patents.sum[patents.sum$cat=="Treated",] # discard controls since we have controls time-series
 
-homesteads.y.train.p <- homesteads.y[homesteads.y$date < "Jul 1898",]
-homesteads.y.test.p <- homesteads.y[homesteads.y$date >= "Jul 1898",]
+homesteads <- reshape(data.frame(patents.sum.treated[c("date","id","homesteads")]), idvar = "date", timevar = "id", direction = "wide")
 
-sales.x.train.p <- sales[sales$date %in% sales.y$date & sales$date < "Jul 1898",]
-sales.x.test.p <- sales[sales$date %in% sales.y$date & sales$date >= "Jul 1898",]
+sales <- reshape(data.frame(patents.sum.treated[c("date","id","sales")]), idvar = "date", timevar = "id", direction = "wide")
 
-sales.y.train.p <- sales.y[sales.y$date < "Jul 1898",]
-sales.y.test.p <- sales.y[sales.y$date >= "Jul 1898",]
+# Labels
+
+homesteads.y <- cats.sums.r[c("date", "homesteads.Control")]
+homesteads.y <- homesteads.y[!is.na(homesteads.y$homesteads.Control),]
+
+sales.y <- cats.sums.r[c("date", "sales.Control")]
+sales.y <- sales.y[!is.na(sales.y$sales.Control),]
+
+# Splits 
+
+homesteads.x.train <- homesteads[homesteads$date >= "Jun 1866" & homesteads$date <= "May 1876",]
+homesteads.x.test <- homesteads[homesteads$date >= "Jun 1876"  & homesteads$date <= "Mar 1889",]
+
+homesteads.y.train <- homesteads.y[homesteads.y$date %in% homesteads.x.train$date & homesteads.y$date >= "Jun 1866" & homesteads.y$date <= "May 1876",]
+homesteads.y.test <- homesteads.y[homesteads.y$date %in% homesteads.x.test$date & homesteads.y$date >= "Jun 1876"  & homesteads.y$date <= "Mar 1889",]
+
+sales.x.train <- sales[sales$date >= "Jun 1866" & sales$date <= "May 1876",]
+sales.x.test <- sales[sales$date >= "Jun 1876" & sales$date <= "Mar 1889",]
+
+sales.y.train <- sales.y[sales.y$date %in% sales.x.train$date & sales.y$date >= "Jun 1866" & sales.y$date <= "May 1876",]
+sales.y.test <- sales.y[sales.y$date %in% sales.x.test$date &sales.y$date >= "Jun 1876" & sales.y$date <= "Mar 1889",]
 
 # Preprocess
-homesteads.pre.train.p <- preProcess(homesteads.x.train.p[!colnames(homesteads.x.train.p) %in% c("date")], method = c("center", "scale","medianImpute"))
-homesteads.x.train.p[!colnames(homesteads.x.train.p) %in% c("date")] <- predict(homesteads.pre.train.p, homesteads.x.train.p[!colnames(homesteads.x.train.p) %in% c("date")] )
+homesteads.pre.train <- preProcess(homesteads.x.train[!colnames(homesteads.x.train) %in% c("date")], method = c("center", "scale","medianImpute"))
+homesteads.x.train[!colnames(homesteads.x.train) %in% c("date")] <- predict(homesteads.pre.train, homesteads.x.train[!colnames(homesteads.x.train) %in% c("date")] )
 
-homesteads.x.test.p[!colnames(homesteads.x.test.p) %in% c("date")] <- predict(homesteads.pre.train.p, homesteads.x.test.p[!colnames(homesteads.x.test.p) %in% c("date")] ) # use training values for test set 
+homesteads.x.test[!colnames(homesteads.x.test) %in% c("date")] <- predict(homesteads.pre.train, homesteads.x.test[!colnames(homesteads.x.test) %in% c("date")] ) # use training values for test set 
 
-sales.pre.train.p <- preProcess(sales.x.train.p[!colnames(sales.x.train.p) %in% c("date")], method = c("center", "scale","medianImpute"))
-sales.x.train.p[!colnames(sales.x.train.p) %in% c("date")] <- predict(sales.pre.train.p, sales.x.train.p[!colnames(sales.x.train.p) %in% c("date")] )
+sales.pre.train <- preProcess(sales.x.train[!colnames(sales.x.train) %in% c("date")], method = c("center", "scale","medianImpute"))
+sales.x.train[!colnames(sales.x.train) %in% c("date")] <- predict(sales.pre.train, sales.x.train[!colnames(sales.x.train) %in% c("date")] )
 
-sales.x.test.p[!colnames(sales.x.test.p) %in% c("date")] <- predict(sales.pre.train.p, sales.x.test.p[!colnames(sales.x.test.p) %in% c("date")] ) # use training values for test set 
+sales.x.test[!colnames(sales.x.test) %in% c("date")] <- predict(sales.pre.train, sales.x.test[!colnames(sales.x.test) %in% c("date")] ) # use training values for test set 
 
 # Export each as csv (labels, features)
-data.directory <- "~/Dropbox/github/drnns-prediction/data/patents/"
+data.directory <- "~/Dropbox/github/drnns-prediction/data/patents-public/analysis-12/control/"
 
-write.csv(homesteads.x.train.p[!colnames(homesteads.x.train.p) %in% c("date")], paste0(data.directory,"homesteads-x-train-placebo.csv"), row.names=FALSE) 
-write.csv(homesteads.x.test.p[!colnames(homesteads.x.test.p) %in% c("date")], paste0(data.directory,"homesteads-x-test-placebo.csv"), row.names=FALSE) 
-write.csv(homesteads.y.train.p[!colnames(homesteads.y.train.p) %in% c("date")], paste0(data.directory,"homesteads-y-train-placebo.csv"), row.names=FALSE) 
-write.csv(homesteads.y.test.p[!colnames(homesteads.y.test.p) %in% c("date")], paste0(data.directory,"homesteads-y-test-placebo.csv"), row.names=FALSE) 
+write.csv(homesteads.x.train[!colnames(homesteads.x.train) %in% c("date")], paste0(data.directory,"homesteads-x-train.csv"), row.names=FALSE) 
+write.csv(homesteads.x.test[!colnames(homesteads.x.test) %in% c("date")], paste0(data.directory,"homesteads-x-test.csv"), row.names=FALSE) 
+write.csv(homesteads.y.train[!colnames(homesteads.y.train) %in% c("date")], paste0(data.directory,"homesteads-y-train.csv"), row.names=FALSE) 
+write.csv(homesteads.y.test[!colnames(homesteads.y.test) %in% c("date")], paste0(data.directory,"homesteads-y-test.csv"), row.names=FALSE) 
 
-write.csv(sales.x.train.p[!colnames(sales.x.train.p) %in% c("date")], paste0(data.directory,"sales-x-train-placebo.csv"), row.names=FALSE) 
-write.csv(sales.x.test.p[!colnames(sales.x.test.p) %in% c("date")] , paste0(data.directory,"sales-x-test-placebo.csv"), row.names=FALSE) 
-write.csv(sales.y.train.p[!colnames(sales.y.train.p) %in% c("date")], paste0(data.directory,"sales-y-train-placebo.csv"), row.names=FALSE) 
-write.csv(sales.y.test.p[!colnames(sales.y.test.p) %in% c("date")], paste0(data.directory,"sales-y-test-placebo.csv"), row.names=FALSE) 
+write.csv(sales.x.train[!colnames(sales.x.train) %in% c("date")], paste0(data.directory,"sales-x-train.csv"), row.names=FALSE) 
+write.csv(sales.x.test[!colnames(sales.x.test) %in% c("date")] , paste0(data.directory,"sales-x-test.csv"), row.names=FALSE) 
+write.csv(sales.y.train[!colnames(sales.y.train) %in% c("date")], paste0(data.directory,"sales-y-train.csv"), row.names=FALSE) 
+write.csv(sales.y.test[!colnames(sales.y.test) %in% c("date")], paste0(data.directory,"sales-y-test.csv"), row.names=FALSE) 
 
-# Save workspace
-save.image("/Users/jason/Dropbox/ok-lottery-local/data/patents/glo-patents-county.RData")
+## Analysis 3: Effect on treated (south), intervention: Mar 1889; period: Jul 1876 - Oct 1976
+
+# Controls are MO counties
+
+# Reshape long to wide
+
+patents.sum.control <- patents.sum[patents.sum$cat=="Control",] # discard treated since we have treated time-series
+
+homesteads <- reshape(data.frame(patents.sum.control[c("date","id","homesteads")]), idvar = "date", timevar = "id", direction = "wide")
+
+sales <- reshape(data.frame(patents.sum.control[c("date","id","sales")]), idvar = "date", timevar = "id", direction = "wide")
+
+# Labels
+
+homesteads.y <- cats.sums.r[c("date", "homesteads.Treated")]
+homesteads.y <- homesteads.y[!is.na(homesteads.y$homesteads.Treated),]
+
+sales.y <- cats.sums.r[c("date", "sales.Treated")]
+sales.y <- sales.y[!is.na(sales.y$sales.Treated),]
+
+# Splits % ADJUST DATES
+
+homesteads.y.train <- homesteads.y[homesteads.y$date >= "Jun 1866" & homesteads.y$date <= "May 1876",]
+homesteads.y.test <- homesteads.y[homesteads.y$date >= "Jun 1876"  & homesteads.y$date <= "Mar 1889",]
+
+homesteads.x.train <- homesteads[homesteads$date %in% homesteads.y.train$date & homesteads$date >= "Jun 1866" & homesteads$date <= "May 1876",]
+homesteads.x.test <- homesteads[homesteads$date %in% homesteads.y.test$date & homesteads$date >= "Jun 1876"  & homesteads$date <= "Mar 1889",]
+
+sales.y.train <- sales.y[sales.y$date >= "Jun 1866" & sales.y$date <= "May 1876",]
+sales.y.test <- sales.y[sales.y$date >= "Jun 1876" & sales.y$date <= "Mar 1889",]
+
+sales.x.train <- sales[sales$date %in% sales.y.train$date & sales$date >= "Jun 1866" & sales$date <= "May 1876",]
+sales.x.test <- sales[sales$date %in% sales.y.test$date & sales$date >= "Jun 1876" & sales$date <= "Mar 1889",]
+
+# Preprocess
+homesteads.pre.train <- preProcess(homesteads.x.train[!colnames(homesteads.x.train) %in% c("date")], method = c("center", "scale","medianImpute"))
+homesteads.x.train[!colnames(homesteads.x.train) %in% c("date")] <- predict(homesteads.pre.train, homesteads.x.train[!colnames(homesteads.x.train) %in% c("date")] )
+
+homesteads.x.test[!colnames(homesteads.x.test) %in% c("date")] <- predict(homesteads.pre.train, homesteads.x.test[!colnames(homesteads.x.test) %in% c("date")] ) # use training values for test set 
+
+sales.pre.train <- preProcess(sales.x.train[!colnames(sales.x.train) %in% c("date")], method = c("center", "scale","medianImpute"))
+sales.x.train[!colnames(sales.x.train) %in% c("date")] <- predict(sales.pre.train, sales.x.train[!colnames(sales.x.train) %in% c("date")] )
+
+sales.x.test[!colnames(sales.x.test) %in% c("date")] <- predict(sales.pre.train, sales.x.test[!colnames(sales.x.test) %in% c("date")] ) # use training values for test set 
+
+# Export each as csv (labels, features)
+data.directory <- "~/Dropbox/github/drnns-prediction/data/patents-public/analysis-34/treated/"
+
+write.csv(homesteads.x.train[!colnames(homesteads.x.train) %in% c("date")], paste0(data.directory,"homesteads-x-train.csv"), row.names=FALSE) 
+write.csv(homesteads.x.test[!colnames(homesteads.x.test) %in% c("date")], paste0(data.directory,"homesteads-x-test.csv"), row.names=FALSE) 
+write.csv(homesteads.y.train[!colnames(homesteads.y.train) %in% c("date")], paste0(data.directory,"homesteads-y-train.csv"), row.names=FALSE) 
+write.csv(homesteads.y.test[!colnames(homesteads.y.test) %in% c("date")], paste0(data.directory,"homesteads-y-test.csv"), row.names=FALSE) 
+
+write.csv(sales.x.train[!colnames(sales.x.train) %in% c("date")], paste0(data.directory,"sales-x-train.csv"), row.names=FALSE) 
+write.csv(sales.x.test[!colnames(sales.x.test) %in% c("date")] , paste0(data.directory,"sales-x-test.csv"), row.names=FALSE) 
+write.csv(sales.y.train[!colnames(sales.y.train) %in% c("date")], paste0(data.directory,"sales-y-train.csv"), row.names=FALSE) 
+write.csv(sales.y.test[!colnames(sales.y.test) %in% c("date")], paste0(data.directory,"sales-y-test.csv"), row.names=FALSE) 
+
+## Analysis 4: Effect on controls (Missouri), intervention: Mar 1889; period: Jul 1876 - Oct 1976
+
+# Reshape long to wide
+
+patents.sum.treated <- patents.sum[patents.sum$cat=="Treated",] # discard controls since we have controls time-series
+
+homesteads <- reshape(data.frame(patents.sum.treated[c("date","id","homesteads")]), idvar = "date", timevar = "id", direction = "wide")
+
+sales <- reshape(data.frame(patents.sum.treated[c("date","id","sales")]), idvar = "date", timevar = "id", direction = "wide")
+
+# Labels
+
+homesteads.y <- cats.sums.r[c("date", "homesteads.Control")]
+homesteads.y <- homesteads.y[!is.na(homesteads.y$homesteads.Control),]
+
+sales.y <- cats.sums.r[c("date", "sales.Control")]
+sales.y <- sales.y[!is.na(sales.y$sales.Control),]
+
+# Splits % ADJUST DATES
+
+homesteads.y.train <- homesteads.y[homesteads.y$date >= "Jun 1866" & homesteads.y$date <= "May 1876",]
+homesteads.y.test <- homesteads.y[homesteads.y$date >= "Jun 1876"  & homesteads.y$date <= "Mar 1889",]
+
+homesteads.x.train <- homesteads[homesteads$date %in% homesteads.y.train$date & homesteads$date >= "Jun 1866" & homesteads$date <= "May 1876",]
+homesteads.x.test <- homesteads[homesteads$date %in% homesteads.y.test$date & homesteads$date >= "Jun 1876"  & homesteads$date <= "Mar 1889",]
+
+sales.y.train <- sales.y[sales.y$date >= "Jun 1866" & sales.y$date <= "May 1876",]
+sales.y.test <- sales.y[sales.y$date >= "Jun 1876" & sales.y$date <= "Mar 1889",]
+
+sales.x.train <- sales[sales$date %in% sales.y.train$date & sales$date >= "Jun 1866" & sales$date <= "May 1876",]
+sales.x.test <- sales[sales$date %in% sales.y.test$date & sales$date >= "Jun 1876" & sales$date <= "Mar 1889",]
+
+# Preprocess
+homesteads.x.train <- homesteads[homesteads$date >= "Jun 1866" & homesteads$date <= "May 1876",]
+homesteads.x.test <- homesteads[homesteads$date >= "Jun 1876"  & homesteads$date <= "Mar 1889",]
+
+homesteads.y.train <- homesteads.y[homesteads.y$date %in% homesteads.x.train$date & homesteads.y$date >= "Jun 1866" & homesteads.y$date <= "May 1876",]
+homesteads.y.test <- homesteads.y[homesteads.y$date %in% homesteads.x.test$date & homesteads.y$date >= "Jun 1876"  & homesteads.y$date <= "Mar 1889",]
+
+sales.x.train <- sales[sales$date >= "Jun 1866" & sales$date <= "May 1876",]
+sales.x.test <- sales[sales$date >= "Jun 1876" & sales$date <= "Mar 1889",]
+
+sales.y.train <- sales.y[sales.y$date %in% sales.x.train$date & sales.y$date >= "Jun 1866" & sales.y$date <= "May 1876",]
+sales.y.test <- sales.y[sales.y$date %in% sales.x.test$date &sales.y$date >= "Jun 1876" & sales.y$date <= "Mar 1889",]
+
+# Export each as csv (labels, features)
+data.directory <- "~/Dropbox/github/drnns-prediction/data/patents-public/analysis-34/control/"
+
+write.csv(homesteads.x.train[!colnames(homesteads.x.train) %in% c("date")], paste0(data.directory,"homesteads-x-train.csv"), row.names=FALSE) 
+write.csv(homesteads.x.test[!colnames(homesteads.x.test) %in% c("date")], paste0(data.directory,"homesteads-x-test.csv"), row.names=FALSE) 
+write.csv(homesteads.y.train[!colnames(homesteads.y.train) %in% c("date")], paste0(data.directory,"homesteads-y-train.csv"), row.names=FALSE) 
+write.csv(homesteads.y.test[!colnames(homesteads.y.test) %in% c("date")], paste0(data.directory,"homesteads-y-test.csv"), row.names=FALSE) 
+
+write.csv(sales.x.train[!colnames(sales.x.train) %in% c("date")], paste0(data.directory,"sales-x-train.csv"), row.names=FALSE) 
+write.csv(sales.x.test[!colnames(sales.x.test) %in% c("date")] , paste0(data.directory,"sales-x-test.csv"), row.names=FALSE) 
+write.csv(sales.y.train[!colnames(sales.y.train) %in% c("date")], paste0(data.directory,"sales-y-train.csv"), row.names=FALSE) 
+write.csv(sales.y.test[!colnames(sales.y.test) %in% c("date")], paste0(data.directory,"sales-y-test.csv"), row.names=FALSE) 
