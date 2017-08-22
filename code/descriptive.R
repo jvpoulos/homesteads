191e906fbca2adacd36154e857ad6d82b790c707
jvpoulos <jvpoulos@gmail.com>
2017-08-22 12:01:35 -0400

    new plots

--- code/descriptive.R
+++ code/descriptive.R
@@ -7,6 +7,7 @@ library(ggplot2)
 library(dplyr)
 library(reshape)
 library(reshape2)
+library(tidyr)
 
 ## Plot patents time series
 
@@ -63,6 +64,93 @@ homesteads.state.time <- ggplot(cats.sums, aes( date, homesteads ,color=cat )) +
                        labels=c("Outside South", "South"))
 
 ggsave(paste0(results.directory,"plots/homesteads-state-time.png"), homesteads.state.time, width=11, height=8.5)
+
+## Plot acres time series
+
+# By time
+
+acres.tab <- patents %>%
+  filter(state_code %in% pub.states) %>% # only public land states
+  filter(authority_code %in% c(251101, 272002)) %>% # homesteads, homesteads
+  group_by(date,authority_code) %>%
+  summarise_each(funs(sum),total_acres) 
+
+acres.tab <- spread(acres.tab, key = authority_code, value = total_acres)
+
+colnames(acres.tab) <- c("Date","Homesteads", "Sales")
+
+acres.tab$Date <- as.POSIXct(acres.tab$Date,format="%m/%d/%Y",tz="UTC")
+
+acres.tab$Homesteads[is.na(acres.tab$Homesteads)] <- 0 # NAs to 0
+acres.tab$Sales[is.na(acres.tab$Sales)] <- 0 # NAs to 0
+
+acres.time <- ggplot(acres.tab, aes(x=Date,y=cumsum(Sales))) + 
+  geom_line(aes(colour='Sales')) +
+  geom_line(aes(y=cumsum(Homesteads),colour='Homesteads')) +
+  coord_cartesian(xlim=as.POSIXct(c("01/01/1800","12/31/1950"), format="%m/%d/%Y",tz="UTC")) +
+  scale_y_continuous(name="Cumulative acres", labels = scales::comma) +
+  xlab("") +
+  scale_colour_manual(name="Patent type",
+                      values=c(Sales="red", Homesteads="blue"))
+
+ggsave(paste0(results.directory,"plots/acres-time.png"), acres.time, width=11, height=8.5)
+
+# By time x region (sales)
+
+acres.tab <- patents %>%
+  filter(state_code %in% pub.states) %>% # only public land states
+  filter(authority_code %in% c(251101, 272002)) %>% # homesteads, sales
+  group_by(date,state_code,authority_code) %>%
+  summarise_each(funs(sum),total_acres) 
+
+acres.tab <- spread(acres.tab, key = authority_code, value = total_acres)
+
+acres.tab$cat <- ifelse(acres.tab$state_code %in% southern.pub, "Treated", "Control") 
+
+colnames(acres.tab) <- c("Date","State", "Homesteads", "Sales", "cat")
+
+# Create control and treated sums
+cats.sums.acres <- acres.tab %>% 
+  group_by(Date,cat) %>% 
+  summarise_each(funs(sum),Sales,Homesteads) 
+
+cats.sums.acres$Date <- as.POSIXct(cats.sums.acres$Date,format="%m/%d/%Y",tz="UTC")
+
+cats.sums.acres.r <- reshape(data.frame(cats.sums.acres), idvar = "Date", timevar = "cat", direction = "wide")
+
+cats.sums.acres.r[, 2:7][is.na(cats.sums.acres.r[, 2:7])] <- 0 # NAs to 0
+
+sales.acres.time <- ggplot(cats.sums.acres.r, aes( Date, cumsum(Sales.Control))) + 
+  geom_line(aes(colour='Control')) +
+  geom_line(aes(y=cumsum(Sales.Treated),colour='Treated')) +
+  coord_cartesian(xlim=as.POSIXct(c("01/01/1800","12/31/1930"), format="%m/%d/%Y",tz="UTC")) +
+  geom_vline(xintercept=c(as.numeric(as.POSIXct("1866-06-01 06:00:00",tz="UTC"))), linetype=2) + # Southern HSA signed
+  geom_vline(xintercept=c(as.numeric(as.POSIXct("1876-06-01 06:00:00",tz="UTC"))), linetype=2) + # Southern HSA repealed
+  geom_vline(xintercept=c(as.numeric(as.POSIXct("1889-03-01 06:00:00",tz="UTC"))), linetype=5) +  
+  scale_y_continuous(name="Cumulative total acres by cash entry", labels = scales::comma) +
+  xlab("") +
+  scale_color_discrete(name="State type",
+                     #  values=c(Control="red", Treated="blue"),
+                       labels=c("Non-southern public land", "Southern public land"))
+
+ggsave(paste0(results.directory,"plots/sales-acres-time.png"), sales.acres.time, width=11, height=8.5)
+
+# By time x state (homesteads)
+
+homesteads.acres.time <- ggplot(cats.sums.acres.r, aes( Date, cumsum(Homesteads.Control))) + 
+  geom_line(aes(colour='Control')) +
+  geom_line(aes(y=cumsum(Homesteads.Treated),colour='Treated')) +
+  coord_cartesian(xlim=as.POSIXct(c("01/01/1862","12/31/1930"), format="%m/%d/%Y",tz="UTC")) +
+  geom_vline(xintercept=c(as.numeric(as.POSIXct("1866-06-01 06:00:00",tz="UTC"))), linetype=2) + # Southern HSA signed
+  geom_vline(xintercept=c(as.numeric(as.POSIXct("1876-06-01 06:00:00",tz="UTC"))), linetype=2) + # Southern HSA repealed
+  geom_vline(xintercept=c(as.numeric(as.POSIXct("1889-03-01 06:00:00",tz="UTC"))), linetype=5) +  
+  scale_y_continuous(name="Cumulative total acres by homestead entry", labels = scales::comma) +
+  xlab("") +
+  scale_color_discrete(name="State type",
+                       #  values=c(Control="red", Treated="blue"),
+                       labels=c("Non-southern public land", "Southern public land"))
+
+ggsave(paste0(results.directory,"plots/homesteads-acres-time.png"), homesteads.acres.time, width=11, height=8.5)
 
 ## Plot capacity time-series
 
