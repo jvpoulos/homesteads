2d8a49224ffa94dd5fcb4de78a55b88ff4658299
jvpoulos <jvpoulos@gmail.com>
2017-09-14 11:56:39 -0400

    railroad capacity plot

--- code/rr-capacity.R
+++ code/rr-capacity.R
@@ -5,18 +5,8 @@
 library(ggplot2)
 library(reshape2)
 library(dplyr)
-
-ggplotRegression <- function (fit) {
-  require(ggplot2)
-  ggplot(fit$model, aes_string(x = names(fit$model)[2], y = names(fit$model)[1])) + 
-    geom_point() +
-    stat_smooth(method = "lm", col = "red") +
-    labs(title = paste0("y = ",signif(fit$coef[[1]],2 ), " + ",
-                        signif(fit$coef[[2]], 2), "x, ",
-                        " p-value = ",signif(summary(fit)$coef[2,4], 2), ", ",
-                        " R^2 = ",signif(summary(fit)$r.squared, 2), ", ",
-                        " N = ",summary(fit)$df[2]+summary(fit)$df[1]))
-}
+library(mgcv)
+library(wesanderson)
 
 # Reshape funds
 
@@ -35,12 +25,41 @@ rr.inter.m.state$track2 <- rr.inter.m.state$cumulative.track/rr.inter.m.state$AR
 
 # merge funds with rr data
 
-rr.funds <- merge(rr.inter.m.state[rr.inter.m.state$InOpBy>=1865,],  # post-CW obs
-                  funds.m[funds.m$year >= 1865,], 
+rr.funds <- merge(rr.inter.m.state,  
+                  funds.m, 
                   by.x =c("state","InOpBy"),
                   by.y = c("state","year"))
 
-# Scatter plot (each point state/year observation) # add lines for each category
+# Scatter plot (each point state/year observation) 
 
-ggplot(rr.funds, aes(track2, value)) + 
-  geom_point(aes(colour = factor(category)))
+LmEq <- function(m) {
+  
+  l <- list(a = format(coef(m)[1], digits = 2),
+            b = format(abs(coef(m)[2]), digits = 2),
+            p.val = format(summary(m)[[4]][2], digits = 2));
+  
+  if (coef(m)[2] >= 0)  {
+    eq <- substitute(italic(y) == a + b %.% italic(x)*","~~italic(p)~"="~p.val,l)
+  } else {
+    eq <- substitute(italic(y) == a - b %.% italic(x)*","~~italic(p)~"="~p.val,l)    
+  }
+  
+  as.character(as.expression(eq));                 
+}
+
+rr.capacity <- ggplot(rr.funds, aes(track2, value, color = factor(category))) + 
+  geom_point() +
+  geom_smooth(data=subset(rr.funds, category=="rev.pc"),se=TRUE, colour=wes_palette("FantasticFox")[3],size=1) +
+  annotate("text", x = 0.27, y = 4.8, label = LmEq(gam(value ~ track2, data=subset(rr.funds, category=="rev.pc"))), colour=wes_palette("FantasticFox")[3], size = 4, parse=TRUE) +
+  geom_smooth(data=subset(rr.funds, category=="exp.pc"),se=TRUE, colour=wes_palette("Moonrise3")[2],size=1) +
+  annotate("text", x = 0.25, y = 0.2, label = LmEq(gam(value ~ track2, data=subset(rr.funds, category=="exp.pc"))), colour=wes_palette("Moonrise3")[2], size = 4, parse=TRUE) +
+  coord_cartesian(ylim=c(0,25)) +
+  scale_colour_manual(name="State capacity measure",
+                      values=c(exp.pc=wes_palette("Moonrise3")[2], rev.pc=wes_palette("FantasticFox")[3]),
+                      label=c("Per-capita revenue",
+                              "Per-capita expenditure")) +
+  ylab("State capacity") +
+  xlab("Railroad rack miles per square mile") 
+ # ggtitle("Railroad access vs. state capacity, 1830-1911 (states)") 
+
+ggsave(paste0(results.directory,"plots/rr-capacity.png"), rr.capacity, width=11, height=8.5)
